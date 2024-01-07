ba8d72a805c2de05a16763191567ee9f4557b751
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-07 17:34:22 +0600

    Caesar Cypher done

--- Caesar_Cypher/main.py
+++ Caesar_Cypher/main.py
@@ -1,45 +1,32 @@
+from logo import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
+def ceaser(text, shift, direction):
+  if direction == "decode":
+    shift = -shift
+  elif direction != "encode":
+    print("Invalid Option selected!")
+    return
 
-
-
-def encrypt(text, shift):
-  newletter = ""
+  newWord = ""
   for letter in text:
-    newletter += alphabet[(alphabet.index(letter) + shift) % 26]
-  print(newletter)
-
-def decrypt(text, shift):
-  newletter = ""
-  for letter in text:
-    index = (alphabet.index(letter) - shift) % 26
-    if(index >= 0):
-      newletter +=  alphabet[index]
+    if letter in alphabet:
+      index = (alphabet.index(letter) + shift) % 26
+      if(index >= 0):
+          newWord +=  alphabet[index]
+      else:
+        newWord += alphabet[26+index]
     else:
-      newletter += alphabet[26+index]
+      newWord += letter
 
-  print(newletter)
+  print(f"Your {direction} message is: {newWord}")
 
-if direction.lower() == "encode":
-  encrypt(text, shift)
-else:
-  decrypt(text, shift)
-    
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+repeat = True
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+while repeat:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  ceaser(text, shift, direction)
+  repeat = input("Do you want to repeat? yes/no: ").lower() == "yes"
