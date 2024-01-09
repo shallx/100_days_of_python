d292a9f9f2c6e98a5acefe76292702cbafb28aa0
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-09 19:14:43 +0600

    Guessing game perfected

--- Guessing_game/main.py
+++ Guessing_game/main.py
@@ -1,36 +1,56 @@
 import random
 import art
-print(art.logo)
+import os
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100")
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard:'").lower()
 
-attemts = 2
+
+
 EASY_ATTEMPTS = 10
 HARD_ATTEMPTS = 5
 
 
-while not (difficulty == "easy" or difficulty == "hard"):
-    print("Invalid selection! Try again")
+
+
+def play():
+    cls()
+    won = False
+    print(art.logo)
+
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard:'").lower()
+    while not (difficulty == "easy" or difficulty == "hard"):
+        print("Invalid selection! Try again")
+        difficulty = input("Choose a difficulty. Type 'easy' or 'hard:'").lower()
 
-difficulty = EASY_ATTEMPTS if "easy" else HARD_ATTEMPTS # Ternary operator
+    attempts = EASY_ATTEMPTS if difficulty == "easy" else HARD_ATTEMPTS # Ternary operator
 
-number = random.randint(1,101)
+    number = random.randint(1,101)
 
-while attemts > 0:
-    print(f"You have {attemts} attempts remaining to guess the number.")
-    guess = int(input("Make a guess:"))
 
-    if guess > number:
-        print("Too high!")
-    elif guess < number:
-        print("Too low")
-    else:
-        print(f"You guessed it, the number is {number}")
-        attemts = 0
+    while attempts > 0:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        guess = int(input("Make a guess:"))
 
-    attemts -= 1
+        if guess > number:
+            print("Too high!")
+        elif guess < number:
+            print("Too low")
+        else:
+            won = True
+            attempts = 0
+
+        attempts -= 1
+
+    print((f"You guessed it!" if won == True else "You lost!") + f" The number was {number}")
+
+    shouldRestart = input("Want to start again? y/n:").lower() == "y"
+    if shouldRestart:
+        play()
+
+play()
+
 
