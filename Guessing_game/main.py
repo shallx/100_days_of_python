590134ddbce468a38b8299b266d074f940316aad
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-09 18:06:18 +0600

    Workin on bug fix

--- Guessing_game/main.py
+++ Guessing_game/main.py
@@ -7,18 +7,21 @@ print("I'm thinking of a number between 1 and 100")
 
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard:'").lower()
 
-attemts = 10
+attemts = 2
+EASY_ATTEMPTS = 10
+HARD_ATTEMPTS = 5
 
-while not (difficulty == "easy" and not difficulty == "hard"):
+
+while not (difficulty == "easy" or difficulty == "hard"):
     print("Invalid selection! Try again")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard:'").lower()
 
-difficulty = 10 if "easy" else 5 # Ternary operator
+difficulty = EASY_ATTEMPTS if "easy" else HARD_ATTEMPTS # Ternary operator
 
 number = random.randint(1,101)
 
 while attemts > 0:
-    print("You have 10 attempts remaining to guess the number.")
+    print(f"You have {attemts} attempts remaining to guess the number.")
     guess = int(input("Make a guess:"))
 
     if guess > number:
@@ -28,6 +31,6 @@ while attemts > 0:
     else:
         print(f"You guessed it, the number is {number}")
         attemts = 0
-        
+
     attemts -= 1
 
