3d09fec98d704499f030015a7657abb48adaf45b
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-11 17:21:56 +0600

    coffee maker 2 done

--- Coffe_maker2/main.py
+++ Coffe_maker2/main.py
@@ -3,4 +3,22 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu = Menu()
-print(menu.get_items())
+coffeeMaker = CoffeeMaker()
+moneyMachine = MoneyMachine()
+
+
+end = False
+while not end:
+    userChoice = input(f"What would you like? ({menu.get_items()}): ").lower()
+
+    if userChoice == "report":
+        print(coffeeMaker.report())
+    elif userChoice == "off":
+        end = True
+    elif userChoice in [item.name for item in menu.menu]:
+        item = menu.find_drink(userChoice)
+        if coffeeMaker.is_resource_sufficient(item):
+            if moneyMachine.make_payment(item.cost):
+                coffeeMaker.make_coffee(item)
+    else:
+        print("Invalid Choice, try again!")
