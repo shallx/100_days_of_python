faeb9534365ce517f12e9138c6467acbadc9f215
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-16 14:40:35 +0600

    Mail Merger done

--- Mail_Merger/main.py
+++ Mail_Merger/main.py
@@ -6,3 +6,20 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+# Read names
+names = []
+with open("./Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+    print(names)
+
+# Read Sample letter
+with open("./Input/Letters/starting_letter.txt") as file:
+    text = file.read()
+
+for name in names:
+    name = name.strip("\n")
+    new_text = text.replace("[name]", name)
+    with open(f"./ReadyToSend/{name}.txt", mode="w") as file:
+        file.write(new_text)
