fb2cc87412bda4cde46ca3fb05fd32c0ef125d27
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-18 03:00:32 +0600

    Tiker example done

--- Tkinter_Playground/main.py
+++ Tkinter_Playground/main.py
@@ -4,18 +4,22 @@ window = tk.Tk()
 window.title("GUI Program")
 window.minsize(width=500, height=300)
 
-my_label = tk.Label(text="My label", font=("Ariel", 24, "bold"))
-my_label.pack()
 
-# Button
 def buttonClick():
     my_label["text"] = input.get()
     
-
+# Label
+my_label = tk.Label(text="My label", font=("Ariel", 24, "bold"))
+my_label.grid(column=0, row=0)
+    
+# Button
 my_button = tk.Button(text="Click me", command=buttonClick)
-my_button.pack()
+my_button.grid(column=1, row=1)
+
+my_button2 = tk.Button(text="Click me 2", command=buttonClick)
+my_button2.grid(column=2, row=0)
 
 # Entry
 input = tk.Entry(width=14)
-input.pack()
+input.grid(column=3, row=3)
 window.mainloop()
