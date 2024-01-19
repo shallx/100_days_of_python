9dc811b6812467be69e2d3f6b366a9c53b055eda
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-19 17:46:05 +0600

    Exception code added for nato alphabet

--- NATO_Alphabet/main.py
+++ NATO_Alphabet/main.py
@@ -6,6 +6,13 @@ nato = {row["letter"]:row["code"] for (_, row) in df.iterrows()}
 
 print(nato)
 
-name = input("Namaywa:").upper()
-print([nato[l] for l in name])
+def get_name():
+    try:
+        name = input("Namaywa:").upper()
+        print([nato[l] for l in name])
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        get_name()
 
+
+get_name()
