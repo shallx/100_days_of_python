91e8bff0429eb13d6530f0f80c9fdb8b35eadf25
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-19 10:16:51 +0600

    Ui done

--- Tkinker_Password_Manager/main.py
+++ Tkinker_Password_Manager/main.py
@@ -11,9 +11,23 @@ window.config(bg="white", padx=20, pady=20)
 window.title("Password Manager")
 
 canvas = tk.Canvas(width=200, height=200, bg="white", highlightthickness=0)
-logo = tk.PhotoImage(file="logo.png", width=200, height=200)
+logo = tk.PhotoImage(file="logo.png")
 canvas.create_image(100,100, image=logo)
-# canvas.grid(row=0, column=0)
-canvas.pack()
+canvas.grid(row=0, column=0)
+
+# Label
+website_label = tk.Label(text="Website:",fg="black", bg="white").grid(row=1, column=0)
+email_label = tk.Label(text="Email/Username:",fg="black", bg="white").grid(row=2, column=0)
+password_label = tk.Label(text="Password:",fg="black", bg="white").grid(row=3, column=0)
+
+
+# Entry
+website_entry = tk.Entry(width=35,fg="black", bg="white").grid(row=1, column=1, columnspan=2)
+email_entry = tk.Entry(width=35,fg="black", bg="white").grid(row=2, column=1, columnspan=2)
+password_entry = tk.Entry(width=21,fg="black", bg="white").grid(row=3, column=1)
+
+# Button
+generate_button = tk.Button(text="Generate Password").grid(row=3, column=2)
+add_button = tk.Button(text="Add", width=36).grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
