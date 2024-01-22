f048116f289c1d9e98d48094b6246df3da1e1e8a
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-22 18:07:22 +0600

    password search added

--- Tkinker_Password_Manager/main.py
+++ Tkinker_Password_Manager/main.py
@@ -4,6 +4,21 @@ import pyperclip
 from password import PasswordGenerator
 import json
 
+
+def search_password():
+    site = website_entry.get()
+
+    try:
+        with open("data.json", mode="r") as file:
+                data = json.load(file)
+                messagebox.showinfo(title=f"Data Found!", message=f"Email:{data[site]["email"]}\nPassword:{data[site]["password"]}")
+                
+    except FileNotFoundError:
+        messagebox.showinfo(title="File Not Found!", message="Data file not found!")
+    except Exception as e:
+        print(e)
+        messagebox.showinfo(title="Website not found", message="No such website found in database")
+        
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def on_generate():
@@ -31,14 +46,11 @@ def on_save():
     else:
         try:
             with open("data.json", mode="r") as file:
-                print("Before loading")
                 data = json.load(file)
-                print(" i am in try")   
         
         except FileNotFoundError:
             with open("data.json", mode="w") as file:
                 json.dump(json_data, file, indent=4)
-                print(" i am in except")
         except json.JSONDecodeError:
             messagebox.showinfo(title="data.json file is corrupted", message="It is either empty, or json file not properly formatted!")
                 
@@ -70,15 +82,16 @@ password_label = tk.Label(text="Password:",fg="black", bg="white").grid(row=3, c
 
 
 # Entry
-website_entry = tk.Entry(width=35,fg="black", bg="white")
-website_entry.grid(row=1, column=1, columnspan=2)
-email_entry = tk.Entry(width=35,fg="black", bg="white")
+website_entry = tk.Entry(width=21,fg="black", bg="white")
+website_entry.grid(row=1, column=1)
+email_entry = tk.Entry(width=38,fg="black", bg="white")
 email_entry.grid(row=2, column=1, columnspan=2)
 password_entry = tk.Entry(width=21,fg="black", bg="white")
 password_entry.grid(row=3, column=1)
 
 # Button
-generate_button = tk.Button(text="Generate Password", command=on_generate).grid(row=3, column=2)
-add_button = tk.Button(text="Add", width=36, command=on_save).grid(row=4, column=1, columnspan=2)
+generate_button = tk.Button(text="Generate Password", command=on_generate, highlightthickness=0).grid(row=3, column=2)
+add_button = tk.Button(text="Add", width=36, command=on_save, highlightthickness=0).grid(row=4, column=1, columnspan=2)
+search_button = tk.Button(text="Search", width=13, command=search_password, highlightthickness=0).grid(row=1, column=2)
 
 window.mainloop()
