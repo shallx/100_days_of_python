f739136e8fc9d58cde1e0a2df04b9685d155716a
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-23 15:56:40 +0600

    flash card done

--- Tkinter_Flash_Card/card_manager.py
+++ Tkinter_Flash_Card/card_manager.py
@@ -1,12 +1,37 @@
 import random
 import pandas
+from enum import Enum
+
+class WritingType(Enum):
+    WRITE = 1
+    UPDATE = 2
 
 class Card_Manager:
     def __init__(self):
-        self.data = pandas.read_csv("./data/ja.csv")
+        try:
+            self.data = pandas.read_csv("./data/words_to_learn.csv")
+        except FileNotFoundError:
+            self.data = pandas.read_csv("./data/ja.csv")
+
         self.data = self.data.to_dict(orient="records")
+        self.pickOne()
+
 
     def pickOne(self):
         index = random.randint(0,len(self.data)-1)
-        item = self.data.pop(index)
-        return (item["English"], item["Japanese"])
+        self.current_word = {"english": self.data[index]["English"], "trans": self.data[index]["Japanese"], "index": index}
+
+        return self.current_word
+    
+    def has_learned(self):
+        item = self.data.pop(self.current_word["index"])
+        item = [{
+            "English" : item["English"],
+            "Japanese" : item["Japanese"]
+        }]
+        self.write_csv("learned", item, writing_type=WritingType.UPDATE, header=False)
+        self.write_csv("words_to_learn", self.data, writing_type=WritingType.WRITE, header=True)
+    
+    def write_csv(self, file_name, data, writing_type=WritingType.WRITE, header: bool = True):
+        dataFrame = pandas.DataFrame.from_records(data)
+        dataFrame.to_csv(f"./data/{file_name}.csv", index=False, mode="w" if writing_type == WritingType.WRITE else "a", header=header)
--- Tkinter_Flash_Card/main.py
+++ Tkinter_Flash_Card/main.py
@@ -9,24 +9,27 @@ FLIP_TIME = 3000
 
 
 # Variables
-lang = {}
+# lang = {}
     
+
+def guessed_correct():
+    card_manager.has_learned()
+    next_language()
 
 # Counter
 def flip_card():
     '''Flips card to show english word by changing background'''
-    global lang
     canvas.itemconfig(canvas_image, image=card_back)
     canvas.itemconfig(title_text, text="English", fill="white")
-    canvas.itemconfig(word_text, text=lang[0], fill="white")
+    canvas.itemconfig(word_text, text=card_manager.current_word["english"], fill="white")
 
 def next_language():
-    global lang, flip_timer
+    global flip_timer
     window.after_cancel(flip_timer)
-    lang = card_manager.pickOne()
+    card_manager.pickOne()
     canvas.itemconfig(canvas_image, image=card_front)
     canvas.itemconfig(title_text, text="Japanese", fill="black")
-    canvas.itemconfig(word_text, text=lang[1], fill="black")
+    canvas.itemconfig(word_text, text=card_manager.current_word["trans"], fill="black")
     flip_timer = window.after(FLIP_TIME, flip_card)
 
 window = tk.Tk()
@@ -36,7 +39,7 @@ right_img = tk.PhotoImage(file="./images/right.png")
 wrong_img = tk.PhotoImage(file="./images/wrong.png")
 card_front = tk.PhotoImage(file="./images/card_front.png")
 card_back = tk.PhotoImage(file="./images/card_back.png")
-right_button = tk.Button(image=right_img, highlightthickness=0, border=0, command=next_language)
+right_button = tk.Button(image=right_img, highlightthickness=0, border=0, command=guessed_correct)
 wrong_button = tk.Button(image=wrong_img, highlightthickness=0, border=0, command=next_language)
 
 canvas = tk.Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
@@ -51,10 +54,8 @@ wrong_button.grid(row=1, column=0)
 right_button.grid(row=1, column=1)
 
 card_manager = Card_Manager()
-# card_manager.print()
-lang = card_manager.pickOne()
 canvas.itemconfig(title_text, text="Japanese")
-canvas.itemconfig(word_text, text=lang[1])
+canvas.itemconfig(word_text, text=card_manager.current_word["trans"])
 
 flip_timer = window.after(FLIP_TIME, flip_card)
 next_language()
