4d1518d9f2d8bcfd9ee630a46444f193d0aa7499
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-23 18:53:40 +0600

    Bithday wisher done

--- SMTP_Birthday_Wisher/main.py
+++ SMTP_Birthday_Wisher/main.py
@@ -1,19 +1,36 @@
-##################### Hard Starting Project ######################
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+FROM_EMAIL = "enter_email_from"
+TO_EMAIL = "enter_email_to"
+PASSWORD = "enter_pass"
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(row['month'], row['day']): row for _, row in data.iterrows()}
+
+
+
+today = dt.datetime.now()
+if (today.month, today.day) in birthdays_dict:
+    print("I found a perfect match")
+    dict = birthdays_dict[(today.month, today.day)]
+    print(f"name: {dict["name"]} | email: {dict.email}")
+
+    with open(f"./letter_templates/letter_{random.randint(1,3)}.txt") as letter:
+        content = letter.read()
+        new_content = content.replace("[NAME]", dict["name"])
+        print(new_content)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=FROM_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=FROM_EMAIL, to_addrs=dict["email"], msg=f"Subject: Happy Birthday\n\n{new_content}")
+
 
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
