9682b01ca12715d4d3a89ed1af809d850327ab3a
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-26 01:06:57 +0600

    sheet update mechanism added

--- SMTP_Flight_Deals/data_manager.py
+++ SMTP_Flight_Deals/data_manager.py
@@ -1,3 +1,16 @@
+import gspread
+SHEET_TITLE = "Flight Deals"
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.client = gspread.service_account(filename="credentials.json")
+        
+        self.wb_1 = self.client.open(SHEET_TITLE)
+        self.worksheet = self.wb_1.get_worksheet(0)
+        # self.worksheet.update('A11', [[1, 2], [3, 4]])
+    
+    def update(self,range, value):
+        cell_list = self.worksheet.range(range)
+        for cell in cell_list:
+            cell.value = "Rahi"
+        self.worksheet.update_cells(cell_list)
+        
--- SMTP_Flight_Deals/main.py
+++ SMTP_Flight_Deals/main.py
@@ -1,1 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from data_manager import DataManager
+
+data_man = DataManager()
+
+data_man.update('B11:B14', [2,3,4,5])
