f47ca18315cb8057ebe31ef7ba1a6101f3a507f8
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-01-29 16:56:30 +0600

    have worked on scrapping_amazon_ptracker

---
+++ Scrapping_Amazon_PTracker/main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+product_link = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
+
+headers = {
+    "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "Accept-Language" : "en-US,en;q=0.9"
+}
+
+response = requests.get(product_link, headers=headers)
+contents = response.text
+soup = BeautifulSoup(contents, 'html.parser')
+print(soup.select_one(".a-price-whole").getText())
---
+++ Scrapping_Songs/main.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+print(CLIENT_ID)
+print(CLIENT_SECRET)
+
+date = input("Which year do you want to travel to? Type the date in this formet: YYYY-MM-DD: ")
+url = f"https://www.billboard.com/charts/hot-100/{date}"
+
+response = requests.get(url=url)
+contents = response.text
+
+soup = BeautifulSoup(contents, 'html.parser')
+
+songs = [titleElement.string.strip() for titleElement in soup.select(".o-chart-results-list-row .c-title")]
+print(songs)
+
+# Connecting with Spotify
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope="playlist-modify-private", redirect_uri="https://www.exampleurl.org", show_dialog=True,
+        cache_path="token.txt",),)
+# sp.user_playlist_create()
+print(sp.current_user())
