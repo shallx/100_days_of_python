a6e8d7514bb86e8b44e1a51a31caf984c837e395
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-02-02 16:19:15 +0600

    Cooky clicker done

--- Selenium_Cookie_Clicker/main.py
+++ Selenium_Cookie_Clicker/main.py
@@ -10,27 +10,37 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.get(url=URL)
 cookie = driver.find_element(By.ID, value="cookie")
-timeout = time.time() + 3
 
-# Finding prices
-items = driver.find_elements(By.CSS_SELECTOR, value="#store div b")
-items.pop(-1)
-prices = [int(item.text.split("-")[1].strip().replace(",", "")) for item in items]
 
 
 def to_number(str : str):
-    return str.strip().replace(",", "")
+    return int(str.strip().replace(",", ""))
 
 def buy_something():
     money = to_number(driver.find_element(by=By.ID, value="money").text)
+    items = driver.find_elements(By.CSS_SELECTOR, value="#store div b")
+    items.pop(-1)
+    # prices = [{"price": to_number(item.text.split("-")[1]), "item" : item} for item in items]
+
+    print(money)
+    try:
+        for item in items[:-1]:
+            item_price = to_number(item.text.split("-")[1])
+            if money >= item_price:
+                item.click()
+                money-=item_price
+    except Exception as e:
+        print(e.__context__)
     # if pric
 
 
-while 1:
 
+while 1:
+    timeout = time.time() + 3
     for i in range(500):
         cookie.click()
         if time.time() > timeout:
+            buy_something()
             break
 
 
