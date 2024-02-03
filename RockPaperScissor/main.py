3d5bde1b18e20d2d81009ba65b4a8b69df6725c5
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2024-02-03 15:20:32 +0600

    Rock paper scissors added

---
+++ RockPaperScissor/main.py
@@ -0,0 +1,55 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+
+
+
+while(1):
+  user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Siccors\n"))
+  
+  if user_choice >= 0 and user_choice <=2:
+    choices = [rock, paper, scissors]
+    print(choices[user_choice])
+
+    computer_choice = random.randint(0,2)
+    print(f"Computer chose:\n {choices[computer_choice]}")
+
+    # - for loose, 1 for win, 0 for draw
+    wincon = [
+      [0, -1, 1], # wincon for rock
+      [1, 0, -1],
+      [-1, 1, 0]
+    ]
+
+    win_outputs = ["Draw", "You Win", "You loose"]
+
+    print(f"{win_outputs[wincon[user_choice][computer_choice]]}\n\n")
+
+  else:
+    print("You choose an invalid number")
--- Tkinter_Pomodoro/main.py
+++ Tkinter_Pomodoro/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,8 +17,10 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 timer = None
+timer_is_running = False
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
     # To stop timer we need to call after_cancel method.
     window.after_cancel(timer)
@@ -28,8 +31,7 @@ def reset_timer():
     reps = 0
 
 
-
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global reps
     reps += 1
@@ -47,22 +49,38 @@ def start_timer():
         title_label.config(text="Work", fg=GREEN)
         count_down(work_sec)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def pause_start_toggle():
+    global timer_is_running
+    if timer_is_running:
+        timer_is_running = False
+        pause_button.config(text="Resume")
+    else:
+        timer_is_running = True
+        pause_button.config(text="Pause")
+        # resume_timer()
+
+
+# def resume_timer():
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def popup_showmessage(title, message):
     # Show app on top
-    window.attributes('-topmost', 1)
+    window.attributes("-topmost", 1)
     messagebox.showinfo(title, message)
-    window.attributes('-topmost', 0) 
-    
+    window.attributes("-topmost", 0)
+
+
 def count_down(count):
     global timer
     count = int(count)
-    count_min = count//60
+    count_min = count // 60
     count_sec = count % 60
 
     if count_sec < 10:
         count_sec = f"0{count_sec}"
-    
+
     if count_min < 10:
         count_min = f"0{count_min}"
 
@@ -72,18 +90,17 @@ def count_down(count):
     else:
         start_timer()
         marks = ""
-        work_sessions = reps//2
+        work_sessions = reps // 2
 
         for _ in range(work_sessions):
             marks += "✓"
         check_label.config(text=marks)
-        
+
         if reps % 2 == 1:
             popup_showmessage("Breaktime Over", "Get back to work")
         else:
             popup_showmessage("Worktime Over", "Time to take break")
-        
-        
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
@@ -96,20 +113,47 @@ title_label.grid(row=0, column=1)
 
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img) # for some reason, i can use tk.PhotoImage directly here, needed variable declaration
-timer_text = canvas.create_text(100,130, text="00:00", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(
+    100, 112, image=tomato_img
+)  # for some reason, i can use tk.PhotoImage directly here, needed variable declaration
+timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_button = tk.Button(text="Start", bg=GREEN, highlightthickness=0, bd=0, padx=0, pady=0, command=start_timer)
+start_button = tk.Button(
+    text="Start",
+    bg=GREEN,
+    highlightthickness=0,
+    bd=0,
+    padx=0,
+    pady=0,
+    command=start_timer,
+)
 start_button.grid(row=2, column=0)
 
-reset_button = tk.Button(text="Reset", bg=GREEN, highlightthickness=0, bd=0, command=reset_timer, padx=0, pady=0)
+pause_button = tk.Button(
+    text="Resume",
+    bg=GREEN,
+    highlightthickness=0,
+    bd=0,
+    padx=0,
+    pady=0,
+    command=pause_start_toggle,
+)
+pause_button.grid(row=2, column=1)
+
+reset_button = tk.Button(
+    text="Reset",
+    bg=GREEN,
+    highlightthickness=0,
+    bd=0,
+    command=reset_timer,
+    padx=0,
+    pady=0,
+)
 reset_button.grid(row=2, column=2)
 
 check_label = tk.Label(text="", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 16, "bold"))
 check_label.grid(row=3, column=1)
-
-
 
 
 window.mainloop()
